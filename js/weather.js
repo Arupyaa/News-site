5f939416725c7362f998a0ad14b2b54c826fe39f
Arupyaa <amkeshk98@gmail.com>
2026-02-28 15:39:47 +0200

    added horizontal aside to home page on mobile screen

--- js/weather.js
+++ js/weather.js
@@ -3,6 +3,13 @@ let weatherApiCityName = "Cairo";
 let weatherApiCountryName = "EG";
 let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherApiCityName},${weatherApiCountryName}&appid=${apiKey}&units=metric`;
 
+let weatherTemp = document.getElementsByClassName("weather-temp");
+weatherTemp = Array.from(weatherTemp);
+let weatherLocation = document.getElementsByClassName("weather-location");
+weatherLocation = Array.from(weatherLocation);
+let weatherIcons = document.querySelectorAll(".weather-img-block img");
+console.log(weatherIcons);
+
 //debug code
 // console.log('test');
 // console.log('test');
@@ -23,9 +30,10 @@ async function homePageInit() {
         weatherJson = JSON.parse(weatherJson);
     }
 
-    document.getElementsByClassName("weather-temp")[0].innerText = weatherJson.main.temp;
-    document.getElementsByClassName("weather-location")[0].innerText = weatherJson.name;
-    document.querySelector(".weather-img-block img").setAttribute("src", `https://openweathermap.org/payload/api/media/file/${weatherJson.weather[0].icon}.png`);
+    weatherTemp.forEach((node) => node.innerText = weatherJson.main.temp);
+    weatherLocation.forEach((node) => node.innerText = weatherJson.name);
+    let weatherIconCode = weatherJson.weather[0].icon;
+    weatherIcons.forEach((node) => node.setAttribute("src", `https://openweathermap.org/payload/api/media/file/${weatherIconCode}.png`));
 }
 
 async function getWeatherFromApi() {
