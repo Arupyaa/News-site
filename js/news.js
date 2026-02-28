0a0562cc7a005280e43cebd473de33dded1cdec6
Arupyaa <amkeshk98@gmail.com>
2026-02-28 20:16:03 +0200

    improved updateNewsElement to be generic for all news sections

--- js/news.js
+++ js/news.js
@@ -1,4 +1,4 @@
-let newsUrl = "https://newsdata.io/api/1/latest?apikey=pub_0546c8c8de6c4c79913850dd3d5a7843"
+let newsUrl = "https://newsdata.io/api/1/latest?apikey=pub_0546c8c8de6c4c79913850dd3d5a7843&removeduplicate=1"
 
 function buildNewsUrl(country, category, language = "en", searchword = undefined) {
     // let words = searchword.trim('').split(' ');
@@ -12,22 +12,32 @@ function buildNewsUrl(country, category, language = "en", searchword = undefined
     }
 }
 
-let politicsImages = document.querySelectorAll(".politics .img-section img");
-let politicsTitles = document.querySelectorAll(".politics h3");
-let politicsDescription = document.querySelectorAll(".politics .card-text");
-let politicsDate = document.querySelectorAll(".politics .date");
-let politicsLogos = document.querySelectorAll(".politics .card-body img");
 
-updateNewsElement();
+updateNewsElement("politics",false);
+updateNewsElement("sports",false);
+updateNewsElement("entertainment",false);
 
 //politics section
-async function updateNewsElement() {
-    let politicsNews = await fetch(buildNewsUrl("eg", "politics")).then((result) => result.json());
+async function updateNewsElement(category,isForceUpdate) {
+    let NewsImages = document.querySelectorAll(`.${category} .img-section img`);
+    let NewsTitles = document.querySelectorAll(`.${category} h3`);
+    let NewsDescription = document.querySelectorAll(`.${category} .card-text`);
+    let NewsDate = document.querySelectorAll(`.${category} .date`);
+    let NewsLogos = document.querySelectorAll(`.${category} .card-body img`);
+    
+    let news = localStorage.getItem(`news${category}Json`);
+    if(news == null || isForceUpdate){
+        news = await fetch(buildNewsUrl("gb", category,"en")).then((result) => result.json());
+        localStorage.setItem(`news${category}Json`,JSON.stringify(news));
+        console.log(`sent api request for ${category} category to news api`);
+    }else{
+        news = JSON.parse(news);
+    }
 
-    politicsImages.forEach((node, nodeIndex) => node.setAttribute("src", politicsNews.results[nodeIndex].image_url));
-    politicsTitles.forEach((node, nodeIndex) => node.innerText = politicsNews.results[nodeIndex].title);
-    politicsDescription.forEach((node, nodeIndex) => node.innerText = politicsNews.results[nodeIndex].description);
-    politicsDate.forEach((node, nodeIndex) => node.innerText = politicsNews.results[nodeIndex].pubDate); //remove extra details later
-    politicsLogos.forEach((node, nodeIndex) => node.setAttribute("src", politicsNews.results[nodeIndex].source_icon));
+    NewsImages.forEach((node, nodeIndex) => node.setAttribute("src", news.results[nodeIndex].image_url));
+    NewsTitles.forEach((node, nodeIndex) => node.innerText = news.results[nodeIndex].title);
+    NewsDescription.forEach((node, nodeIndex) => node.innerText = news.results[nodeIndex].description);
+    NewsDate.forEach((node, nodeIndex) => node.innerText = news.results[nodeIndex].pubDate); //remove extra details later
+    NewsLogos.forEach((node, nodeIndex) => node.setAttribute("src", news.results[nodeIndex].source_icon));
 }
 
